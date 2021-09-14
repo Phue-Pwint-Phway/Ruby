9698d2043ee4f406ef641506a1d5f6a0301616c5
PHUEPWINTPHWAY\PhuePwintPhway <scm.phuepwintphway@gmail.com>
2021-09-15 02:47:31 +0630

    only create user controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # get 'post/index'
   # get 'login/login'
 
-  root 'post#index'
+  get "users/index"
+  
+  resources :post
+  root to: 'post#index'
+  # root 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
