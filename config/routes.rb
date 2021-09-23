4b599053e0f5d1595c0d2b930307a663352a716f
PHUEPWINTPHWAY\PhuePwintPhway <scm.phuepwintphway@gmail.com>
2021-09-24 01:33:59 +0630

    test for foreign key

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
+# Rails.application.routes.draw do
+#   root "articles#index"
+
+#   get "/articles", to: "articles#index"
+#   get "/articles/:id", to: "articles#show"
+# end
+
 Rails.application.routes.draw do
-  get 'login/login'
-  # get 'posts/index'
-  root 'posts#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "articles#index"
+
+  resources :articles do
+    resources :comments
+  end
 end
